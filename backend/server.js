ef40922a478da65c8c549b4f816802cdb4cb1a52
Jaspreet Singh <itannjass@gmail.com>
2026-03-02 15:03:25 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,13 @@ dotenv.config();
 const app = express()
 
 
+const corsfront={
+    origin:"https://elcto-three.vercel.app/",
+    Credential:true
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsfront))
 
 app.listen(8000, () => {
     console.log("Server is running on 7000")
