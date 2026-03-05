141ddff47ef164513afb0bdb348c9fb57d845801
Jaspreet Singh <itannjass@gmail.com>
2026-03-05 15:03:58 +0530

    up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -272,7 +272,27 @@ export const Main = () => {
 
     return (
         <>
-         
+         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" data-aos="fade">
+                <div class="carousel-inner">
+                    <div class="carousel-item">
+                        <img src={banner1} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={banner2} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={banner3} class="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
            
             <div className="container mt-5" data-aos="zoom-in">
                 <h2 className="fw-bold text-center mb-4">Product Categories</h2>
