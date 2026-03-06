d123680295d067cc5246f232cf2718aeff01c91d
Jaspreet Singh <itannjass@gmail.com>
2026-03-06 19:18:00 +0530

    up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -398,7 +398,7 @@ export const Main = () => {
                                 </div>
 
                                 <div className="card-body p-0">
-                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold">{p.ProductName}</h6>
+                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold product-title">{p.ProductName}</h6>
                                     <div className="mb-2 text-warning">
     <i className="bi bi-star-fill"></i>
     <i className="bi bi-star-fill"></i>
@@ -466,7 +466,7 @@ export const Main = () => {
                                 </div>
 
                                 <div className="card-body p-0">
-                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold">{p.ProductName}</h6>
+                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold product-title">{p.ProductName}</h6>
                                     <div className="mb-2 text-warning">
     <i className="bi bi-star-fill"></i>
     <i className="bi bi-star-fill"></i>
@@ -527,7 +527,7 @@ export const Main = () => {
                                 </div>
 
                                 <div className="card-body p-0">
-                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold">{p.ProductName}</h6>
+                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold product-title">{p.ProductName}</h6>
                                     <div className="mb-2 text-warning">
     <i className="bi bi-star-fill"></i>
     <i className="bi bi-star-fill"></i>
@@ -588,7 +588,7 @@ export const Main = () => {
                                 </div>
 
                                 <div className="card-body p-0">
-                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold">{p.ProductName}</h6>
+                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold product-title">{p.ProductName}</h6>
                                     <div className="mb-2 text-warning">
     <i className="bi bi-star-fill"></i>
     <i className="bi bi-star-fill"></i>
@@ -648,7 +648,7 @@ export const Main = () => {
                                 </div>
 
                                 <div className="card-body p-0">
-                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold">{p.ProductName}</h6>
+                                    <h6 className="fs-6 fs-md-5 fs-lg-5 fw-semibold product-title">{p.ProductName}</h6>
                                     <div className="mb-2 text-warning">
     <i className="bi bi-star-fill"></i>
     <i className="bi bi-star-fill"></i>
