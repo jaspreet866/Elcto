8cd361d84651567ac29382d839143e07e54e0c32
Jaspreet Singh <itannjass@gmail.com>
2026-03-10 16:50:15 +0530

    up

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,14 @@ app.put("/api/changestatus/:id", async (req, res) => {
     }
 })
 
+
+
+const cloudinary=cloudinary.config({
+    cloud_name: process.env.Cloud_name,
+    api_key: process.env.Api_key,
+    api_secret: process.env.Secret_key
+})
+
 // category api
 
 const myStorage = new CloudinaryStorage({
