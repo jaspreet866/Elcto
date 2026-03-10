4bf58e037d5778c543d5cc42805eff8f7037a613
Jaspreet Singh <itannjass@gmail.com>
2026-03-10 16:41:31 +0530

    up

--- frontend/src/cloud.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dmpptipmx",
-  api_key: "433534999489711",
-  api_secret: "sQwBGoVuZyI-m9-x0Ad35OTElyg"
-});
-
-export default cloudinary;
