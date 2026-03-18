4c78bdf5737ed22e08e5af4824e97bfb785413be
Jaspreet Singh <itannjass@gmail.com>
2026-03-18 16:06:18 +0530

    upde

--- frontend/src/wishlist.js
+++ frontend/src/wishlist.js
@@ -151,9 +151,7 @@ export const Wish = () => {
 
                                     <div className="d-flex flex-column flex-md-row gap-1">
 
-                                       <Link to={`/detail?id=${a._id}&cid=${a.Category} `} className="btn btn-primary btn-sm w-100 ">
-                                            View Product
-                                        </Link>
+                                        <button onClick={() => { cart(id, a.Name, a.Price, a.Img, a.Quantity) }} className="btn btn-sm btn-primary mt-auto w-100">Add to Cart </button>
 
                                         <button className="btn btn-sm btn-danger mt-auto w-100" onClick={() => remove(a._id)}>
                                             Remove
