e7bb3bf8036ee5028f9db1a26984634ca0c90a46
Jaspreet Singh <itannjass@gmail.com>
2026-05-07 10:05:28 +0530

    upd1

--- backend/server.js
+++ backend/server.js
@@ -120,7 +120,7 @@ app.post("/api/forgot", async (req, res) => {
         const data = await resend.emails.send({
             from: "onboarding@resend.dev", // default test sender
             to: email,
-           subject: `You requested a password reset. Click this link to reset your password: https://elcto-1.onrender.com/api/verify?email=${email}`,
+           subject: `You requested a password reset`,
            html: `<p>You requested a password reset.</p><p><a href="https://elcto-self.vercel.app/verify?email=${email}">Click here to reset your password</a></p><p>Your OTP is ${otp}</P>`,
         });
 
